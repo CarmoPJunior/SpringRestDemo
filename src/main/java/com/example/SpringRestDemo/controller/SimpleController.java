62b6ebe21ea6bc4c0788b1f1c7017d1df46851fc
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2020-04-06 00:15:47 -0300

    Commit Inicial...

---
+++ src/main/java/com/example/SpringRestDemo/controller/SimpleController.java
@@ -0,0 +1,25 @@
+package com.example.SpringRestDemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+	
+	
+//    @Value("${spring.application.name}")
+//    String appName;
+ 
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", "teste");
+        return "home";
+    }
+    
+    @GetMapping("/sample")
+    public String showForm() {
+        return "sample";
+    }
+}
